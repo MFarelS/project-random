3b8952311b8b3af67019a24f5f8e3b80735798bf
tfkhdyt <ta142fa@gmail.com>
2021-05-05 22:27:36 +0700

    gak penting sih, cuma ganti output aja

--- Java/Mei 2021/tes_perulangan.java
+++ Java/Mei 2021/tes_perulangan.java
@@ -1,8 +1,8 @@
 class tes_perulangan{
   public static void main(String [] args){
-    for(int i = 1; i <= 10; i++){
-      String separator = (i != 10) ? ", " : "";
-      System.out.printf("Hello world %d" + separator + "\n", i);
+    for(int i = 1; i <= 100; i++){
+      String separator = (i != 100) ? ", " : "";
+      System.out.printf(i + separator);
     }
   }
 }
