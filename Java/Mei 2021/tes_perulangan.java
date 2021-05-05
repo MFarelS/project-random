90a8edd8e451ec1707bae0d7a8f876c9cc09d218
tfkhdyt <ta142fa@gmail.com>
2021-05-05 21:55:23 +0700

    nah

--- Java/Mei 2021/tes_perulangan.java
+++ Java/Mei 2021/tes_perulangan.java
@@ -2,7 +2,7 @@ class tes_perulangan{
   public static void main(String [] args){
     for(int i = 1; i <= 10; i++){
       String separator = (i != 10) ? ", " : "";
-      System.out.printf("Hello world %d" + separator + "\n", i)
+      System.out.printf("Hello world %d" + separator + "\n", i);
     }
   }
 }
