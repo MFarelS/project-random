2bd857de76b7f026e27db853a8ff1ea9df631a3a
tfkhdyt <ta142fa@gmail.com>
2021-05-11 13:41:46 +0700

    nyoba concat php

---
+++ PHP/tes_concat.php
@@ -0,0 +1,5 @@
+<?php
+  $namaDepan = readline("Masukkan nama depan : ");
+  $namaBelakang = readline("Masukkan nama belakang : ");
+  echo "\nNama lengkap anda adalah ".$namaDepan." ".$namaBelakang;
+?>
