3f16530ea7bde30bd01ea08cb9ed7ea26f5ade82
tfkhdyt <ta142fa@gmail.com>
2021-05-23 15:56:57 +0700

    nambah swal

--- Javascript/Mei 2021/swal/server.js
+++ Javascript/Mei 2021/swal/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.get('/', function(req, res) {
-  res.sendFile('index.html');
+  res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 app.listen(5000, console.log('Server is running on port 5000'));
