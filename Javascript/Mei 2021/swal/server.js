412399bd40ce7d471dffe41f8bcfab74cab45860
tfkhdyt <ta142fa@gmail.com>
2021-05-23 15:53:23 +0700

    rename ke server.js

--- Javascript/Mei 2021/swal/server.js
+++ Javascript/Mei 2021/swal/server.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const fs = require('fs');
-const sys = require('sys');
 const app = express();
 
-app.get('/', function(req, res){
-  res.render('/index.html');
+app.get('/', function(req, res) {
+  res.sendFile('index.html');
 });
 
 app.listen(5000, console.log('Server is running on port 5000'));
