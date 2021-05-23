157028770ec9c6c3327dbeb5f6d58c93258ee97d
tfkhdyt <ta142fa@gmail.com>
2021-05-24 05:37:11 +0700

    nambah file header

--- Javascript/Mei 2021/belajar-react-npm/src/App.js
+++ Javascript/Mei 2021/belajar-react-npm/src/App.js
@@ -1,17 +1,31 @@
 import logo from './logo.svg';
 import React from 'react';
 import './App.css';
-import Title from './Title';
+
+const Header = () => {
+  return(
+    <div class=row>
+      <div class="mx-auto col-12 col-lg-6">
+        <div class="text-center card my-2">
+          <div class="card-header pb-0 pt-4">
+            <img src={this.props.gambar} height="150px" alt="" class="mx-auto d-block mb-3 rounded-circle"/>
+            <h1 class="mb-0 title">
+              {this.props.nama}
+            </h1>
+            <br/>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Title />
-      </header>
+    <div class="text-center container" style="min-height:100%;">
+      <Header gambar="https://tfkhdyt.web.app/assets/img/me.jpg" nama="Taufik Hidayat"/>
     </div>
-  );
+  )
 }
 
 export default App;
