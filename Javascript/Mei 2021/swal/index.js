b9ec174dd11abf8513c9d80280618f46fd7d0a78
tfkhdyt <ta142fa@gmail.com>
2021-05-23 15:48:44 +0700

    nambah file index.html

--- Javascript/Mei 2021/swal/index.js
+++ Javascript/Mei 2021/swal/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
+const fs = require('fs');
+const sys = require('sys');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World</h1>');
-});
-
-app.get('/:name', (req, res) => {
-  res.send(`<h1>Welcome to ${req.params.name}!</h1>`);
+app.get('/', function(req, res){
+  res.render('/index.html');
 });
 
 app.listen(5000, console.log('Server is running on port 5000'));
