29d35e89cb2fea0af82863b7a2e624752cd92f4b
tfkhdyt <ta142fa@gmail.com>
2021-05-24 07:19:52 +0700

    awokawokawok

--- Javascript/Mei 2021/belajar-react-npm/src/App.js
+++ Javascript/Mei 2021/belajar-react-npm/src/App.js
@@ -1,18 +1,102 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import React from 'react';
 import './App.css';
 
-const Header = () => {
+const copyNomer = (nomor) => {
+  var dummy = document.createElement("textarea");
+  document.body.appendChild(dummy);
+  dummy.value = nomor;
+  dummy.select();
+  document.execCommand("copy");
+  document.body.removeChild(dummy);
+}
+
+const Header = (props) => {
   return(
-    <div class=row>
+    <div class="row">
       <div class="mx-auto col-12 col-lg-6">
         <div class="text-center card my-2">
           <div class="card-header pb-0 pt-4">
-            <img src={this.props.gambar} height="150px" alt="" class="mx-auto d-block mb-3 rounded-circle"/>
+            <img src={props.gambar} height="150px" alt="" class="mx-auto d-block mb-3 rounded-circle" />
             <h1 class="mb-0 title">
-              {this.props.nama}
+              {props.nama}
             </h1>
-            <br/>
+            <br />
+    </div>
+    </div>
+    </div>
+    </div>
+  )
+}
+
+const Tab = (props) => {
+  return(
+    <li class={props.kelasLi} role="presentation">
+    <button class={props.kelasA}
+      aria-controls={props.nama}
+      aria-selected="true"
+      data-bs-target={props.id}
+      data-bs-toggle="tab"
+      id={props.tab}
+      role="tab">{props.title}</button>
+    </li>
+  )
+}
+
+const ThreeButton = (props) => {
+  return(
+    <div>
+    <button onMouseOver={() => copyNomer('0895338865375')}  type="button" class="btn btn-outline-primary mb-1" data-bs-toggle="tooltip" data-bs-placement="top" title="Nomor telah disalin!"><i class="fa fa-mobile-alt"></i> {props.nomor}</button><br/>
+      <button class="btn btn-primary" data-bs-target={props.modal} data-bs-toggle="modal">
+          <i class="fas fa-qrcode"></i> QR Code
+      </button>&nbsp;
+      <a href={props.link} class="btn btn-success" target="_blank" rel="noreferrer"><i class="fas fa-external-link-alt"></i> Buka {props.nama}</a>
+    </div>
+  );
+}
+
+const CardBody = (props) => {
+  return(
+    <div class="fade tab-pane show active" id={props.id} aria-labelledby={props.tab} role="tabpanel">
+      <h5 class="card-title">Support me via {props.nama}</h5>
+      <p class="card-text my-4">
+        <img src="https://tfkhdyt.web.app/assets/img/ovo.svg" alt="" height="50vh"/>
+      </p>
+      <p class="card-text">
+        <ThreeButton nama="OVO"
+                     nomor="0895338865375"
+                     modal="#modalOvo"
+                     link="ovo://transfer"
+                     copy="copyNomer('0895338865375')"
+        />
+      </p>
+    </div>
+  )
+}
+
+const Body = () => {
+  return(
+      <div class="row">
+      <div class="mx-auto col-12 col-lg-6">
+        <div class="text-center card">
+          <div class="card-header">
+            <ul class="card-header-pills nav nav-pills nav-fill" id="myTabs" role="tablist">
+              <Tab kelasLi="nav-item mx-auto" kelasA="nav-link active" nama="ovo" id="#ovo" tab="ovo-tab" title="OVO" />
+              <Tab kelasLi="nav-item" kelasA="nav-link" nama="linkaja" id="#linkaja" tab="linkaja-tab" title="LinkAja" />
+              <Tab kelasLi="nav-item" kelasA="nav-link" nama="dana" id="#dana" tab="dana-tab" title="Dana" />
+              <Tab kelasLi="nav-item" kelasA="nav-link" nama="gopay" id="#gopay" tab="gopay-tab" title="Gopay" />
+              <Tab kelasLi="nav-item" kelasA="nav-link" nama="paypal" id="#paypal" tab="paypal-tab" title="PayPal" />
+              <Tab kelasLi="nav-item" kelasA="nav-link" nama="saweria" id="#saweria" tab="saweria-tab" title="Saweria" />
+              <Tab kelasLi="nav-item" kelasA="nav-link" nama="Trakteer" id="#trakteer" tab="trakteer-tab" title="Trakteer" />
+            </ul>
+          </div>
+          <div class="card-body">
+            <div class="tab-content" id="myTabsContent">
+              <CardBody id="#ovo"
+                        tab="ovo-tab"
+                        nama="OVO"
+              />
+            </div>
           </div>
         </div>
       </div>
@@ -21,11 +105,12 @@ const Header = () => {
 }
 
 function App() {
-  return (
-    <div class="text-center container" style="min-height:100%;">
-      <Header gambar="https://tfkhdyt.web.app/assets/img/me.jpg" nama="Taufik Hidayat"/>
-    </div>
-  )
-}
+    return (
+      <div class="text-center container">
+      <Header gambar="https://tfkhdyt.web.app/assets/img/me.jpg" nama="Taufik Hidayat" />
+      <Body />
+      </div>
+    )
+    }
 
 export default App;
