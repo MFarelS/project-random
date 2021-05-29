acd70a55c35dc5f421c69c1e331ccf962ce11ac8
tfkhdyt <ta142fa@gmail.com>
2021-05-29 21:33:35 +0700

    done hehe

--- Javascript/Mei 2021/kalkulator/kalkulator.js
+++ Javascript/Mei 2021/kalkulator/kalkulator.js
@@ -1,2 +1,6 @@
-let a = readline("Masukkan angka 1 : ")
-let b = readline("Masukkan angka 2 : ")
+const input = require('prompt-sync')()
+console.clear()
+let a = parseInt(input("Masukkan angka 1 : "))
+let b = parseInt(input("Masukkan angka 2 : "))
+let c = a + b
+console.log("Hasilnya adalah  : " + c)
