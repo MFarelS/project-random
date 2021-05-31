3a2f5527a2cdfbe0b33cd6ce609656fb5c69f5c5
tfkhdyt <ta142fa@gmail.com>
2021-05-31 10:50:57 +0700

    gak tau

--- Javascript/Mei 2021/kalkulator/kalkulator.js
+++ Javascript/Mei 2021/kalkulator/kalkulator.js
@@ -1,6 +1,6 @@
 const input = require('prompt-sync')() // npm install prompt-sync
 console.clear()
-let a = parseInt(input("Masukkan angka 1 : "))
-let b = parseInt(input("Masukkan angka 2 : "))
-let c = a + b
+let a = input("Masukkan angka 1 : ")
+let b = input("Masukkan angka 2 : ")
+let c = Number(a) + Number(b)
 console.log("Hasilnya adalah  : " + c)
