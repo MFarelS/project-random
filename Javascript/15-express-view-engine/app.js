fe3b669aada34c3f9b90d186ca17b7783cfb38c8
tfkhdyt <ta142fa@gmail.com>
2021-07-24 06:44:02 +0700

    install ejs

--- Javascript/15-express-view-engine/app.js
+++ Javascript/15-express-view-engine/app.js
@@ -3,16 +3,10 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  //res.json({
-    //nama : 'Taufik',
-    //email : 'tfkhdyt@pm.me',
-    //noHP : '089533'
-  //})
   res.sendFile('./index.html', {root: __dirname})
 })
 
 app.get('/about', (req, res) => {
-  //res.send('Ini adalah halaman about!')
   res.sendFile('./about.html', {root: __dirname})
 })
 
@@ -25,7 +19,6 @@ app.use('/', (req, res) => {
   res.status(404)
   res.send('<h1>404</h1>')
 })
-
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
