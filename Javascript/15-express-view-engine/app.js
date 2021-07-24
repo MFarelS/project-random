226cd566aed87bdbc83c8c3ee1e19b80d32553b3
tfkhdyt <ta142fa@gmail.com>
2021-07-24 07:50:51 +0700

    bikin main layouts

--- Javascript/15-express-view-engine/app.js
+++ Javascript/15-express-view-engine/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
 const app = express()
 const port = 3000
 
 // gunakan ejs
 app.set('view engine', 'ejs');
+app.use(expressLayouts)
 
 app.get('/', (req, res) => {
   const mahasiswa = [
@@ -21,7 +23,7 @@ app.get('/', (req, res) => {
     },
   ]
   res.render('index', {
-    nama : 'Taufik Hidayat', 
+    layout: 'layouts/main-layouts',
     title : 'Halaman home',
     mahasiswa,
   })
@@ -29,7 +31,7 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
   res.render('about', {
-    nama : 'Taufik Hidayat',
+    layout: 'layouts/main-layouts',
     title : "Halaman about"
   })
 })
