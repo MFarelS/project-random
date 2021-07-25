32d353c4f6829c8a10eebf5a6deeabd5e5cd4611
tfkhdyt <ta142fa@gmail.com>
2021-07-25 20:36:24 +0700

    nambah project 19

---
+++ Javascript/19-express-contact-app-3/utils/contacts.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+const dirPath = './data';
+if (!fs.existsSync(dirPath)) {
+  fs.mkdirSync(dirPath);
+}
+
+const dataPath = './data/contacts.json';
+if (!fs.existsSync(dataPath)) {
+  fs.writeFileSync(dataPath, '[]', 'utf-8');
+}
+
+const loadContact = () => {
+  const file = fs.readFileSync('data/contacts.json', 'utf-8');
+  const contacts = JSON.parse(file);
+  return contacts;
+};
+
+const findContact = (nama) => {
+  const contacts = loadContact();
+  const contact = contacts.find((contact) => {
+    return contact.nama.toLowerCase() === nama.toLowerCase()});
+  return contact
+}
+
+const saveContacts = (contacts) => {
+  fs.writeFileSync('data/contacts.json', JSON.stringify(contacts))
+}
+
+const addContact = (contact) => {
+  const contacts = loadContact()
+  contacts.push(contact)
+  saveContacts(contacts)
+}
+
+const cekDuplikat = nama => {
+  const contacts = loadContact()
+  return contacts.find((contact) => {
+    return contact.nama === nama
+  })
+}
+
+module.exports = {loadContact, findContact, addContact, cekDuplikat};
