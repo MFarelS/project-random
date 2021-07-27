ecf36293a0bce0f4a19ecaf3432778925a92aa5b
tfkhdyt <ta142fa@gmail.com>
2021-07-27 12:40:46 +0700

    nambah program hitung nilai

--- Javascript/22-coba-mongodb/app.js
+++ Javascript/22-coba-mongodb/app.js
@@ -1,108 +1,109 @@
-const { MongoClient, ObjectID } = require('mongodb')
+const { MongoClient, ObjectID } = require("mongodb");
 
-const url = 'mongodb://127.0.0.1:27017'
-const dbName = 'taufik'
+const url = "mongodb://127.0.0.1:27017";
+const dbName = "taufik";
 
 const client = new MongoClient(url, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+  useUnifiedTopology: true,
+});
 
 client.connect((error, client) => {
-  if(error){
-    return console.log('Koneksi gagal!')
+  if (error) {
+    return console.log("Koneksi gagal!");
   }
   // pilih database
-  const db = client.db(dbName)
+  const db = client.db(dbName);
 
   // menambahkan 1 data ke collection mahasiswa
   //db.collection('mahasiswa').insertOne({
-    //nama : 'Erik',
-    //email : 'erik@gmail.com'
+  //nama : 'Erik',
+  //email : 'erik@gmail.com'
   //}, (error, result) => {
-    //if(error){
-      //return console.log('Gagal menambahkan data!')
-    //}
-    //console.log(result)
+  //if(error){
+  //return console.log('Gagal menambahkan data!')
+  //}
+  //console.log(result)
   //})
 
   // menambahkan lebih dari satu data
   //db.collection('mahasiswa').insertMany(
-    //[
-      //{
-        //nama : 'Erik',
-        //email : 'erik@yahoo.com'
-      //},
-      //{
-        //nama : 'Afif',
-        //email : 'afif@gmail.com'
-      //}
-    //], (error, result) => {
-      //if(error){
-        //return console.log('Data gagal ditambahkan!')
-      //}
+  //[
+  //{
+  //nama : 'Erik',
+  //email : 'erik@yahoo.com'
+  //},
+  //{
+  //nama : 'Afif',
+  //email : 'afif@gmail.com'
+  //}
+  //], (error, result) => {
+  //if(error){
+  //return console.log('Data gagal ditambahkan!')
+  //}
 
-      //console.log(result)
-    //})
-  
+  //console.log(result)
+  //})
+
   // menampilkan semua data yg ada di collection mahasiswa
   //console.log(db.collection('mahasiswa').find().toArray((error, result) => {
-    //console.log(result)
+  //console.log(result)
   //}))
 
   // menampilkan data berdasarkan kriteria yg ada di collection 'mahasiswa'
   //console.log(
-    //db.collection('mahasiswa')
-    //.find({ _id : ObjectID('60fec69bff7be15f6bd11f44')})
-    //.toArray((error, result) => {
-    //console.log(result)
+  //db.collection('mahasiswa')
+  //.find({ _id : ObjectID('60fec69bff7be15f6bd11f44')})
+  //.toArray((error, result) => {
+  //console.log(result)
   //}))
-  
+
   // mengubah data berdasarkan id
   //const updatePromise = db.collection('mahasiswa').updateOne({
-    //_id : ObjectID("60fec69bff7be15f6bd11f44")
+  //_id : ObjectID("60fec69bff7be15f6bd11f44")
   //}, {
-    //$set: {
-      //email : "avip@yahoo.com"
-    //}
+  //$set: {
+  //email : "avip@yahoo.com"
+  //}
   //})
 
   //updatePromise.then(result => {
-    //console.log(result)
+  //console.log(result)
   //})
   //.catch(err => {
-    //console.log(err)
+  //console.log(err)
   //})
-  
+
   // mengubah data lebih dari 1 berdasarkan kriteria
   //db.collection('mahasiswa').updateMany({
-    //nama : 'Erik'
-  //}, 
+  //nama : 'Erik'
+  //},
   //{
-    //$set : {
-      //nama : 'Erik Doank'
-    //}
+  //$set : {
+  //nama : 'Erik Doank'
+  //}
   //})
 
   // menghapus 1 data
   //db.collection('mahasiswa').deleteOne(
-    //{
-      //_id : ObjectID("60fec69bff7be15f6bd11f44") 
-    //}
+  //{
+  //_id : ObjectID("60fec69bff7be15f6bd11f44")
+  //}
   //).then(result => {
-    //console.log(result)
+  //console.log(result)
   //}).catch(error => {
-    //console.log(error)
+  //console.log(error)
   //})
-  
+
   // menghapus banyak data
-  db.collection('mahasiswa').deleteMany(
-    {
-      nama : 'Erik Doank'
-    }
-  ).then(result => {
-    console.log(result)
-  }).catch(error => {
-    console.log(error)
-  })
-})
+  db.collection("mahasiswa")
+    .deleteMany({
+      nama: "Erik Doank",
+    })
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
---
+++ Javascript/program-hitung-nilai/script.js
@@ -0,0 +1,58 @@
+const data = [];
+const btnSimpan = document.getElementById("btnSimpan");
+const btnTampilkan = document.getElementById("btnTampilkan");
+
+btnSimpan.addEventListener("click", function () {
+  const nama = document.getElementById("nama").value;
+  const nilai = document.getElementById("nilai").value;
+  const elementIndeks = document.getElementById("indeks");
+  const elementKeterangan = document.getElementById("keterangan");
+  
+  let indeks;
+  let keterangan;
+  if (nilai >= 80 && nilai <= 100) {
+    indeks = "A";
+    keterangan = 'Sangat Baik';
+  } else if (nilai >= 68 && nilai <= 79) {
+    indeks = "B";
+    keterangan = 'Baik';
+  } else if (nilai >= 20 && nilai <= 67) {
+    indeks = "C";
+    keterangan = 'Buruk';
+  } else {
+    elementIndeks.value = 'Nilai tidak valid!';
+    return false;
+  }
+  elementIndeks.value = indeks;
+  elementKeterangan.value = keterangan;
+  data.push({nama,nilai,indeks,keterangan});
+});
+
+btnTampilkan.addEventListener('click', function () {
+  const nim = document.getElementById("nim").value;
+  const elementOutput = document.getElementById('output');
+  let html = `
+    <b>NIM : ${nim}</b>
+    <table border='1' cellspacing='0' style='border-collapse:collapse; margin-top: 20px;'>
+      <tr>
+        <th>No</th>
+        <th>Nama Mata Kuliah</th>
+        <th>Nilai Akhir</th>
+        <th>Indeks</th>
+        <th>Keterangan</th>
+      </tr>
+  `
+  data.forEach((e, i) => {
+    html += `
+      <tr>
+        <td>${i+1}</td>
+        <td>${e.nama}</td>
+        <td>${e.nilai}</td>
+        <td>${e.indeks}</td>
+        <td>${e.keterangan}</td>
+      </tr>
+    `
+  });
+  html += '</table>'
+  elementOutput.innerHTML = html
+});
