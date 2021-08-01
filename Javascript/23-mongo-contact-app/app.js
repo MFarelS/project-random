570205b08ddfce620216e03c7ab00bda18f58094
tfkhdyt <ta142fa@gmail.com>
2021-08-01 20:12:44 +0700

    nambah project 23, mungkin

--- Javascript/23-mongo-contact-app/app.js
+++ Javascript/23-mongo-contact-app/app.js
@@ -1,1 +1,31 @@
-ya
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  const mahasiswa = [
+    {
+      nama: "Taufik",
+      email: "tfkhdyt@pm.me",
+    },
+    {
+      nama: "Fauzi",
+      email: "fauzi@pm.me",
+    },
+    {
+      nama: "Doni",
+      email: "doni@pm.me",
+    },
+  ];
+  res.render("index", {
+    layout: "layouts/main-layout",
+    title: "Home",
+    mahasiswa,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Mongo Contact App | Listening at http://localhost:${port}`);
+});
