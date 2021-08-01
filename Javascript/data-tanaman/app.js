6acc12d61d84548130ac8a63253686c3a67d4af2
tfkhdyt <ta142fa@gmail.com>
2021-08-01 21:15:03 +0700

    nambah dependensi buat flash message

--- Javascript/data-tanaman/app.js
+++ Javascript/data-tanaman/app.js
@@ -1,16 +1,35 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
-
 const port = 3000
 
 // View engine setup
-app.set('view engine', 'ejs');
-  
+app.set('view engine', 'ejs');  
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use('/', function(req, res){
-    res.render('login/index')
+app.get('/', function(req, res){
+    res.render('login')
 });
+
+app.get('/dashboard', (req, res) => {
+  res.render('dashboard')
+})
+
+app.post('/auth', (req, res) => {
+  const username = req.body.username
+  const password = req.body.password
+
+  if(username === "taufik"){
+    if(password === "hidayat"){
+      res.redirect('/dashboard')
+    }
+  }else{
+    console.log("Username salah");
+  }
+})
+
   
 app.listen(port, function(err){
     if (err) console.log(err);
