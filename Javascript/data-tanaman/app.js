e9d6c294d25b9240940722097339d78bcdb95a8a
tfkhdyt <ta142fa@gmail.com>
2021-08-01 22:00:12 +0700

    bikin tabel

--- Javascript/data-tanaman/app.js
+++ Javascript/data-tanaman/app.js
@@ -1,37 +1,61 @@
-const express = require('express')
-const bodyParser = require('body-parser');
-const app = express()
-const port = 3000
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const flash = require("connect-flash");
+const app = express();
+const port = 3000;
 
 // View engine setup
-app.set('view engine', 'ejs');  
-app.use(express.static('public'))
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', function(req, res){
-    res.render('login')
+// konfigurasi flash
+app.use(cookieParser("secret"));
+app.use(
+  session({
+    cookie: { maxAge: 6000 },
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(flash());
+
+app.get("/", function (req, res) {
+  res.render("login");
 });
 
-app.get('/dashboard', (req, res) => {
-  res.render('dashboard')
-})
+app.get("/dashboard", (req, res) => {
+  res.render("dashboard");
+});
 
-app.post('/auth', (req, res) => {
-  const username = req.body.username
-  const password = req.body.password
+app.post("/", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
-  if(username === "taufik"){
-    if(password === "hidayat"){
-      res.redirect('/dashboard')
+  if (username === "taufik") {
+    if (password === "hidayat") {
+      res.redirect("/dashboard");
+    } else {
+      req.flash("msg", "Password salah!");
+      res.render("login", {
+        msg: req.flash("msg"),
+      });
     }
-  }else{
+  } else {
     console.log("Username salah");
+    req.flash("msg", "Username salah!");
+    res.render("login", {
+      msg: req.flash("msg"),
+    });
   }
-})
+});
 
-  
-app.listen(port, function(err){
-    if (err) console.log(err);
-    console.log("Server listening on PORT", port);
+app.listen(port, function (err) {
+  if (err) console.log(err);
+  console.log("Server listening on PORT", port);
 });
