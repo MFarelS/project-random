fb478fd4ca333538ff43bdeeb7bb1a1322b026ad
tfkhdyt <ta142fa@gmail.com>
2021-08-18 06:20:17 +0700

    nambah component blogPost dan post

--- Javascript/react-hello-world/src/component/Post.jsx
+++ Javascript/react-hello-world/src/component/Post.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 
-const Post = () => {
+const Post = props => {
   return (
-    <div className="col p-2">
+    <div className="card mx-3 my-4 col-12 col-md-9" style={{boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px'}}>
       <div className='row'>
         <div className='col'>
-          <h5 className="card-title my-1">Judul</h5>
-          <p className="card-text my-1">Ini deskripsi</p>
+          <img src="https://placeimg.com/640/500/tech" alt="..." className='img-fluid'/>
+        </div>
+        <div className="col p-4">
+          <div className='row'>
+            <div className='col'>
+              <h5 className="card-title my-1">{props.data.title}</h5>
+              <p className="card-text my-1">{props.data.body}</p>
+              <button className='btn btn-danger mt-1' onClick={() => props.remove(props.data.id)}>Remove</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- Javascript/react-hello-world/src/container/BlogPost/BlogPost.jsx
+++ Javascript/react-hello-world/src/container/BlogPost/BlogPost.jsx
@@ -1,19 +1,72 @@
 import React, {Component, Fragment} from 'react'
 import Post from "../../component/Post"
+import axios from 'axios'
 
 class BlogPost extends Component {
-  render () {
+  state = {
+    post : [],
+    formBlogPost : {
+      id: 1,
+      title: '',
+      body: '',
+      userId: 1
+    }
+  }
+  
+  getPostAPI = () => {
+    axios.get('http://localhost:3001/posts')
+    .then(res => {
+      this.setState({
+        post : res.data
+      })
+    })
+  }
+  
+  handleRemove = (data) => {
+    console.log(data)
+    axios.delete('http://localhost:3001/posts/' + data)
+    .then((res) => {
+      this.getPostAPI()
+    })
+  }
+  
+  handleFormChange = (e) => {
+    const formBlogSpotNew = {...this.state.formBlogPost}
+    formBlogSpotNew[e.target.name] = e.target.value
+    this.setState({
+      formBlogPost: formBlogSpotNew
+    }, () => {
+      console.log(this.state.formBlogPost)
+    })
+  }
+  
+  componentDidMount(){
+    this.getPostAPI()
+  }
+  
+  render(){
     return (
-    <Fragment>
-      <div className="card ms-2 mt-2" style={{boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px'}}>
-        <div className='row'>
-          <div className='col'>
-            <img src="https://placeimg.com/640/500/tech" alt="..." className='img-fluid'/>
+      <Fragment>
+        <div className='row m-3'>
+          <div className='col-12 col-md-9 p-4' style={{boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px'}}>
+            <div className="form-floating mb-3">
+              <input type="text" className="form-control" id="title" name="title" placeholder="Judul" onChange={this.handleFormChange}/>
+              <label htmlFor="title">Judul</label>
+            </div>
+            <div className="form-floating">
+              <textarea className='form-control' id='body' name="body" placeholder='Body' style={{height:'100px'}} onChange={this.handleFormChange}>
+              </textarea>
+              <label htmlFor="body">Deskripsi</label>
+            </div>
+            <button className='btn btn-success mt-3'>Simpan</button>
           </div>
-          <Post title='' desc=''/>
         </div>
-      </div>
-    </Fragment>
+        {
+          this.state.post.map(post => {
+            return <Post key={post.id} data={post} remove={this.handleRemove}/>
+          })
+        }
+      </Fragment>
     )
   }
 }
