534f1a47b26e7d261f732c25abdca17a143b1cf8
tfkhdyt <ta142fa@gmail.com>
2021-08-18 21:35:24 +0700

    nambah fitur edit data

--- Javascript/react-hello-world/src/component/Post.jsx
+++ Javascript/react-hello-world/src/component/Post.jsx
@@ -2,19 +2,16 @@ import React from 'react'
 
 const Post = props => {
   return (
-    <div className="card mx-3 my-4 col-12 col-md-9" style={{boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px'}}>
+    <div className="card mx-3 my-4 col-11 col-md-9 mx-auto" style={{boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px'}}>
       <div className='row'>
         <div className='col'>
           <img src="https://placeimg.com/640/500/tech" alt="..." className='img-fluid'/>
         </div>
-        <div className="col p-4">
-          <div className='row'>
-            <div className='col'>
-              <h5 className="card-title my-1">{props.data.title}</h5>
-              <p className="card-text my-1">{props.data.body}</p>
-              <button className='btn btn-danger mt-1' onClick={() => props.remove(props.data.id)}>Remove</button>
-            </div>
-          </div>
+        <div className="col m-4 ms-0">
+          <h5 className="card-title my-1">{props.data.title}</h5>
+          <p className="card-text my-1">{props.data.body}</p>
+          <button className='btn btn-primary mt-1 me-2' onClick={() => props.update(props.data)}>Update</button>
+          <button className='btn btn-danger mt-1' onClick={() => props.remove(props.data.id)}>Remove</button>
         </div>
       </div>
     </div>
--- Javascript/react-hello-world/src/container/BlogPost/BlogPost.jsx
+++ Javascript/react-hello-world/src/container/BlogPost/BlogPost.jsx
@@ -10,16 +10,60 @@ class BlogPost extends Component {
       title: '',
       body: '',
       userId: 1
-    }
+    },
+    isUpdate : false
   }
   
   getPostAPI = () => {
-    axios.get('http://localhost:3001/posts')
+    axios.get('http://localhost:3001/posts?_sort=id&_order=desc')
     .then(res => {
       this.setState({
         post : res.data
       })
     })
+  }
+  
+  postDataToAPI = () => {
+    axios.post('http://localhost:3001/posts', this.state.formBlogPost)
+    .then((res) => {
+      console.log(res);
+      this.getPostAPI();
+      this.setState({
+        formBlogPost : {
+          id: 1,
+          title: '',
+          body: '',
+          userId: 1
+        },
+      });
+    }, (err) => {
+      console.log("error : ", err);
+    });
+  }
+  
+  putDataToAPI = () => {
+    axios.put(`http://localhost:3001/posts/${this.state.formBlogPost.id}`, this.state.formBlogPost)
+    .then((res) => {
+      this.getPostAPI();
+      this.setState({
+        isUpdate: false,
+        formBlogPost : {
+          id: 1,
+          title: '',
+          body: '',
+          userId: 1
+        },
+      })
+    }, (err) => {
+      console.log('error: ', err)
+    })
+  }
+  
+  handleUpdate = (data) => {
+    this.setState({
+      formBlogPost : data,
+      isUpdate: true
+    });
   }
   
   handleRemove = (data) => {
@@ -31,13 +75,24 @@ class BlogPost extends Component {
   }
   
   handleFormChange = (e) => {
-    const formBlogSpotNew = {...this.state.formBlogPost}
-    formBlogSpotNew[e.target.name] = e.target.value
+    const formBlogSpotNew = {...this.state.formBlogPost};
+    const timestamp = new Date().getTime();
+    if(!this.state.isUpdate){
+      formBlogSpotNew['id'] = timestamp;
+    }
+    console.log(this.state.isUpdate)
+    formBlogSpotNew[e.target.name] = e.target.value;
     this.setState({
       formBlogPost: formBlogSpotNew
-    }, () => {
-      console.log(this.state.formBlogPost)
-    })
+    });
+  }
+  
+  handleSubmit = () => {
+    if(this.state.isUpdate){
+      this.putDataToAPI();
+    }else{
+      this.postDataToAPI();
+    }
   }
   
   componentDidMount(){
@@ -48,22 +103,22 @@ class BlogPost extends Component {
     return (
       <Fragment>
         <div className='row m-3'>
-          <div className='col-12 col-md-9 p-4' style={{boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px'}}>
+          <div className='col-12 col-md-9 p-4 mx-auto' style={{boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px'}}>
             <div className="form-floating mb-3">
-              <input type="text" className="form-control" id="title" name="title" placeholder="Judul" onChange={this.handleFormChange}/>
+              <input type="text" value={this.state.formBlogPost.title} className="form-control" id="title" name="title" placeholder="Judul" onChange={this.handleFormChange}/>
               <label htmlFor="title">Judul</label>
             </div>
             <div className="form-floating">
-              <textarea className='form-control' id='body' name="body" placeholder='Body' style={{height:'100px'}} onChange={this.handleFormChange}>
+              <textarea value={this.state.formBlogPost.body} className='form-control' id='body' name="body" placeholder='Body' style={{height:'100px'}} onChange={this.handleFormChange}>
               </textarea>
               <label htmlFor="body">Deskripsi</label>
             </div>
-            <button className='btn btn-success mt-3'>Simpan</button>
+            <button className='btn btn-success mt-3' onClick={this.handleSubmit}>Simpan</button>
           </div>
         </div>
         {
           this.state.post.map(post => {
-            return <Post key={post.id} data={post} remove={this.handleRemove}/>
+            return <Post key={post.id} data={post} update={this.handleUpdate} remove={this.handleRemove}/>
           })
         }
       </Fragment>
