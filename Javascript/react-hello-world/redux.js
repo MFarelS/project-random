4877b701988c8aa62b22d18515946c77488262ff
tfkhdyt <ta142fa@gmail.com>
2021-08-21 05:41:30 +0700

    nambah reducer pada redux

--- Javascript/react-hello-world/redux.js
+++ Javascript/react-hello-world/redux.js
@@ -2,12 +2,34 @@ const redux = require('redux');
 const createStore = redux.createStore;
 
 const initialState = {
-  value : 0
+  value : 0,
+  age : 19
 };
 
 const rootReducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case 'ADD_AGE':
+      return {
+        ...state,
+        age : state.age + 1
+      }
+    case 'CHANGE_VALUE':
+      return {
+        ...state,
+        value : state.value + action.newValue
+      }
+    default:
+      return state;
+  }
 };
 
 const store = createStore(rootReducer);
 console.log(store.getState());
+
+store.subscribe(() => {
+  console.log('State changed! :', store.getState());
+});
+
+store.dispatch({type : 'ADD_AGE'});
+store.dispatch({type : 'CHANGE_VALUE', newValue : 10});
+// console.log(store.getState());
